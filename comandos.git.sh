0f1706f133ab675589507664d26dbf9c05ca1f8f
AlexAugustoDocket <alex.augusto@docket.com.br>
2020-05-24 22:52:39 -0300

    inicio

---
+++ comandos.git.sh
@@ -0,0 +1,37 @@
+
+# criar um repo
+git init 
+
+# clonar um repo
+git clone 
+
+# adicionar arquivos no git versionar
+git add [arquivo, ou . para adicionar todos]
+
+# registrar alterações e tornar rastreaveis
+git commit -m 'mensagem'
+
+# baixar mudanças sicronizar com o servidor
+git pull
+
+# enviar mudanças para servidor remoto
+git push
+
+# verifica o estado do repo, mostra as alterações
+git status
+
+# historico de alterações
+git log
+
+# cria branch
+git branch [nome da branch]
+
+# Serve para visualizar a listagem de todas as branchs criadas no repositório local.
+git branch 
+
+# muda de branch
+git checkout [nome da branch]
+
+# cria arquivo
+touch [nome do arquivo]
+
